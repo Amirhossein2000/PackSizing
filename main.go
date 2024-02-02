947c1d5e63f47f84808f35c04f401944d8d03d05
Amirhossein <38152308+Amirhossein2000@users.noreply.github.com>
2024-02-02 20:42:52 +0100

    Init

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func generateCombinations(packSizes []int, targetNumber int) (string, int) {
+	maxCounts := []int{targetNumber / packSizes[0], targetNumber / packSizes[1], targetNumber / packSizes[2]}
+
+	bestCombination := ""
+	bestValue := 0
+
+	for i := 0; i <= maxCounts[0]; i++ {
+		a := i * packSizes[0]
+		if bestValue != 0 && a > bestValue {
+			break
+		}
+
+		for j := 0; j <= maxCounts[1]; j++ {
+			b := j * packSizes[1]
+			if bestValue != 0 && a+b > bestValue {
+				break
+			}
+
+			for k := 0; k <= maxCounts[2]; k++ {
+				c := k * packSizes[2]
+				value := a + b + c
+				if bestValue != 0 && value > bestValue {
+					break
+				}
+
+				if value >= targetNumber && (value < bestValue || bestValue == 0) {
+					bestCombination = fmt.Sprintf("(%dx%d) + (%dx%d) + (%dx%d)", i, packSizes[0], j, packSizes[1], k, packSizes[2])
+					bestValue = value
+				}
+				if targetNumber == value {
+					goto end
+				}
+			}
+		}
+	}
+
+end:
+	return bestCombination, bestValue
+}
+
+func main() {
+	packetSize := []int{53, 31, 23}
+	fmt.Println(generateCombinations(packetSize, 263))
+	fmt.Println(generateCombinations(packetSize, 250))
+	fmt.Println(generateCombinations(packetSize, 500000))
+	fmt.Println(generateCombinations(packetSize, 50000000))
+}
