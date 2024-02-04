4140776977b8ad52d685c8ceb6b1287753a893d6
Amirhossein <38152308+Amirhossein2000@users.noreply.github.com>
2024-02-04 23:22:03 +0100

    Fix lenThreeSolution

--- threepacks.go
+++ threepacks.go
@@ -8,23 +8,23 @@ func lenThreeSolution(packSizes []int, targetNumber int) (string, int) {
 	bestCombination := ""
 	bestValue := 0
 
-	for i := 0; i <= maxCounts[0]; i++ {
+	for i := maxCounts[0]; i >= 0; i-- {
 		a := i * packSizes[0]
 		if bestValue != 0 && a > bestValue {
-			break
+			continue
 		}
 
-		for j := 0; j <= maxCounts[1]; j++ {
+		for j := maxCounts[1]; j >= 0; j-- {
 			b := j * packSizes[1]
 			if bestValue != 0 && a+b > bestValue {
-				break
+				continue
 			}
 
-			for k := 0; k <= maxCounts[2]; k++ {
+			for k := maxCounts[2]; k >= 0; k-- {
 				c := k * packSizes[2]
 				value := a + b + c
 				if bestValue != 0 && value > bestValue {
-					break
+					continue
 				}
 
 				if value >= targetNumber && (value < bestValue || bestValue == 0) {
